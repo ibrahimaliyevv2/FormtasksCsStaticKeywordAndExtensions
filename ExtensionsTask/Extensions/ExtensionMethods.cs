ad4da095a9232f36b7ca85002833e1fddf2bfaf9
ibrahimaliyevv2 <7cauc3b@code.edu.az>
2022-04-04 16:23:38 +0400

    formtasks added

---
+++ ExtensionsTask/Extensions/ExtensionMethods.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensions
+{
+    public static class ExtensionMethods
+    {
+        public static bool IsPrime(this int num)
+        {
+            
+                if (num == 0 || num == 1)
+                {
+                    return false;
+                }
+                for (int i = 2; i * i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        return false;
+                    }
+                }
+         return true;
+        }
+
+        public static bool IsPowOfTwo(this int num)
+        {
+            for (int i = 1; i <= num; i*=2)
+            {
+                if (num == i)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool CustomContains(this string str, string complete)
+        {
+            string[] strArr = complete.Split(" ");
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                if (strArr[i] == str)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool CustomContains(this char ch, string complete)
+        {
+            for (int i = 0; i < complete.Length; i++)
+            {
+                if (complete[i] == ch)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ ExtensionsTask/ExtensionsTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Extensions;
+namespace ExtensionsTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1001;
+            Console.WriteLine(num.IsPrime());
+
+            int num2 = 1024;
+            Console.WriteLine(num2.IsPowOfTwo());
+
+            char pattern2 = 'a';
+            Console.WriteLine(pattern2.CustomContains("Salam"));
+
+            string pattern = "Salam";
+            Console.WriteLine(pattern.CustomContains("Salam necesiz ?"));
+ 
+        }
+    }
+}
